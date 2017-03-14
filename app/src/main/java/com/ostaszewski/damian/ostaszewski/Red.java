ee04d8348c23c8842942ce9df7c6e5e31053b606
kjl-zse <klwp@wp.pl>
2017-03-14 17:47:05 +0100

    Projekt Ostaszewski

---
+++ app/src/main/java/com/ostaszewski/damian/ostaszewski/Red.java
@@ -0,0 +1,19 @@
+package com.ostaszewski.damian.ostaszewski;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by student on 07.03.2017.
+ */
+
+public class Red extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.red);
+
+
+    }
+}
